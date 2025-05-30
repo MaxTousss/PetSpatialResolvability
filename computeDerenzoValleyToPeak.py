7273665366f0577ae8ab6f2f269c362a3e4157b8
mgaudreault <maxime.g.gaudreault@usherbrooke.ca>
2025-05-30 14:46:24 -0400

    Cherry-pick changes from MaxTousss

--- computeDerenzoValleyToPeak.py
+++ computeDerenzoValleyToPeak.py
@@ -867,7 +867,7 @@ class Region:
 		cornerCount = len(self.corners)
 		for i in range(cornerCount):
 			arrow = plt.annotate('', xy=self.corners[i].xy, xytext=self.corners[(i + 1) % cornerCount].xy,
-			                     arrowprops=dict(arrowstyle='->', color=color[i]))
+			                     arrowprops=dict(arrowstyle='<-', mutation_scale=28, color=color[i]))
 			self.arrows.append(arrow)
 
 		self.cidpress = _figure.canvas.mpl_connect('button_press_event', self.onPress)
@@ -1014,7 +1014,8 @@ class SpotCircle:
 		# Expected spot
 		self.spot = plt.Circle(xy=_xy,
 		                       radius=_spotSize / 2.0,
-		                       edgecolor=_color, facecolor='none', zorder=_zorder)
+		                       edgecolor=_color, facecolor='none', zorder=_zorder, 
+		                       linewidth=2.5)
 
 		# Vertices of the triangle
 		self.verticle = plt.Circle(xy=_xy,
